ae05785f68dad5d08ed91da501d9ce17d0c565b7
Pete Smith <roysvork@gmail.com>
2014-09-19 08:52:13 +0100

    Removed some unneccesary bits from MongoDocument

--- src/MongoDB.Driver.Tests/Linq/MongoDocument.cs
+++ src/MongoDB.Driver.Tests/Linq/MongoDocument.cs
@@ -7,53 +7,6 @@
     using MongoDB.Bson.Serialization;
     using MongoDB.Bson.Serialization.Attributes;
     
-    public enum AccessType
-    {
-        Unknown,
-        Allow,
-        ReadOnly,
-        Deny
-    }
-
-    public enum SourceType
-    {
-        Unknown,
-        Parent,
-        Record,
-        App
-    }
-
-    public enum ActorType
-    {
-        Unknown = 0,
-        Team = 1,
-        User = 2,
-        Owner = 3,
-        All = 4
-    }
-
-    public class AccessRight
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
-
-        [BsonRepresentation(BsonType.Int32)]
-        public AccessType AccessType { get; set; }
-
-        [BsonRepresentation(BsonType.Int32)]
-        public SourceType SourceType { get; set; }
-
-        [BsonRepresentation(BsonType.Int32)]
-        public ActorType ActorType { get; set; }
-
-        public string ActorId { get; set; }
-
-        public string ParentId { get; set; }
-
-        public bool Permissive { get; set; }
-    }
-
     [Serializable]
     [BsonSerializer(typeof(MongoDocumentClassSerializer))]
     public class MongoDocument : BsonDocumentBackedClass
@@ -78,8 +31,6 @@
         public string Id { get; set; }
 
         public bool IsArchived { get; set; }
-
-        public List<AccessRight> AccessRights { get; set; }
         
         public BsonValue this[string fieldname]
         {
