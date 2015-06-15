ee692c56d101f5563de1231b2f37f4902c06b33f
Craig Wilson <craiggwilson@gmail.com>
2015-06-15 10:32:32 -0500

    added corrupted bson specification tests.

---
+++ src/MongoDB.Bson.Tests/Specifications/bson/TestRunner.cs
@@ -0,0 +1,91 @@
+﻿/* Copyright 2013-2014 MongoDB Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using FluentAssertions;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using NUnit.Framework;
+
+namespace MongoDB.Bson.Specifications.bson
+{
+    [TestFixture]
+    public class TestRunner
+    {
+        [TestCaseSource(typeof(TestCaseFactory), "GetTestCases")]
+        public void RunTestDefinition(BsonDocument definition)
+        {
+            var encodedHex = (string)definition["encoded"];
+            var bytes = BsonUtils.ParseHexString(encodedHex);
+            using (var stream = new MemoryStream(bytes))
+            using (var reader = new BsonBinaryReader(stream))
+            {
+                var context = BsonDeserializationContext.CreateRoot(reader);
+                if (definition.Contains("error"))
+                {
+                    Action act = () => BsonDocumentSerializer.Instance.Deserialize(context);
+                    act.ShouldThrow<Exception>();
+                }
+            }
+        }
+
+        private static class TestCaseFactory
+        {
+            public static IEnumerable<ITestCaseData> GetTestCases()
+            {
+                const string prefix = "MongoDB.Bson.Tests.Specifications.bson.tests.";
+                return Assembly
+                    .GetExecutingAssembly()
+                    .GetManifestResourceNames()
+                    .Where(path => path.StartsWith(prefix) && path.EndsWith(".json"))
+                    .SelectMany(path =>
+                    {
+                        var definition = ReadDefinition(path);
+                        var fullName = path.Remove(0, prefix.Length);
+
+                        int i = 0;
+                        var dataList = new List<ITestCaseData>();
+                        foreach (BsonDocument document in (BsonArray)definition["documents"])
+                        {
+                            var data = new TestCaseData(document);
+                            data.Categories.Add("Specifications");
+                            data.Categories.Add("bson");
+                            dataList.Add(data
+                                .SetName(fullName.Remove(fullName.Length - 5) + " " + i)
+                                .SetDescription((string)definition["description"] + " " + i));
+                            i++;
+                        }
+
+                        return dataList;
+                    });
+            }
+
+            private static BsonDocument ReadDefinition(string path)
+            {
+                using (var definitionStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path))
+                using (var definitionStringReader = new StreamReader(definitionStream))
+                {
+                    var definitionString = definitionStringReader.ReadToEnd();
+                    return BsonDocument.Parse(definitionString);
+                }
+            }
+        }
+    }
+}
