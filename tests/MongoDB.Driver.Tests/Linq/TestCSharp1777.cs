7043b71b54262f2c88d786a58182fb9bfd037a71
Craig Wilson <craiggwilson@gmail.com>
2017-03-02 17:09:30 -0600

    wip

---
+++ tests/MongoDB.Driver.Tests/Linq/TestCSharp1777.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq.Translators;
+using Xunit;
+
+namespace TestCSharp1777
+{
+    public class Test
+    {
+        public enum E { A, B }
+
+        public class C
+        {
+            public int Id;
+            [BsonRepresentation(BsonType.String)]
+            public E? P;
+        }
+
+        [Fact]
+        public void Weirdness()
+        {
+            var persistentIds = (IEnumerable<E>)(new E[] { E.A, E.B });
+            var pIds = persistentIds.Select(x => x as E?);
+            var result = Project(x => pIds.Contains(x.P));
+        }
+
+        private RenderedProjectionDefinition<TResult> Project<TResult>(Expression<Func<C, TResult>> projector)
+        {
+            return Project(projector, null);
+        }
+
+        private RenderedProjectionDefinition<TResult> Project<TResult>(Expression<Func<C, TResult>> projector, ExpressionTranslationOptions translationOptions)
+        {
+            var serializer = BsonSerializer.SerializerRegistry.GetSerializer<C>();
+            return AggregateProjectTranslator.Translate(projector, serializer, BsonSerializer.SerializerRegistry, translationOptions);
+        }
+    }
+}
